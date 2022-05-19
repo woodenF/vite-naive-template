a575236ee57aae2116ba1693a07e9be284985fc2
emory_feng <916885119@qq.comg>
2022-05-19 18:18:44 +0800

    chore: 配置eslint规则

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80, //单行长度
+  tabWidth: 2, //缩进长度
+  useTabs: false, //使用空格代替tab缩进
+  semi: true, //句末使用分号
+  singleQuote: true, //使用单引号
+  bracketSpacing: true,
+  arrowParens: 'always',
+  trailingComma: 'none'
+};
